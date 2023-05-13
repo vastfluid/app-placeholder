96e2aae3b751226d22d62ed4bb08745a0eae6a62
TaiwoJazz <babsman4all@gmail.com>
2023-05-14 00:20:47 +0100

    minor fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,14 @@
+import Head from 'next/head';
+
 export default function Home() {
   return (
-    <h3 className='text-2xl font-semibold flex items-center justify-center h-screen text-center -mt-16'>
-      Our DEX is currently under development. Stay tuned for updates!
-    </h3>
+    <div>
+      <Head>
+        <title>VastFluid</title>
+      </Head>
+      <h3 className='text-2xl font-semibold flex items-center justify-center h-screen text-center -mt-16'>
+        Our DEX is currently under development. Stay tuned for updates!
+      </h3>
+    </div>
   );
 }
